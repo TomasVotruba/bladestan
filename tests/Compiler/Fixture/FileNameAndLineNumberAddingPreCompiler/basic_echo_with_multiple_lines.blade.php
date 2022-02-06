800a56bab810bfd8cc519d71ae72bdd9bf5449df
Can Vural <can9119@gmail.com>
2022-02-07 00:21:09 +0100

    (pre compiler) Add test case for variable echo with multiple lines

---
+++ tests/Compiler/Fixture/FileNameAndLineNumberAddingPreCompiler/basic_echo_with_multiple_lines.blade.php
@@ -0,0 +1,7 @@
+<div class="output">
+    <h1>{{ $foo }}</h1>
+</div>
+-----
+/** file: foo.blade.php, line: 1 */<div class="output">
+/** file: foo.blade.php, line: 2 */    <h1>{{ $foo }}</h1>
+/** file: foo.blade.php, line: 3 */</div>
