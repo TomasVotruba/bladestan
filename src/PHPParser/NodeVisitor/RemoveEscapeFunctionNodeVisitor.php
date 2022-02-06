a26a40416d9efcf265e7f839f08f797973f7fcac
Can Vural <can9119@gmail.com>
2022-02-07 00:20:01 +0100

    RemoveEscapeFunctionNodeVisitor can take out the comments above echo statement

--- src/PHPParser/NodeVisitor/RemoveEscapeFunctionNodeVisitor.php
+++ src/PHPParser/NodeVisitor/RemoveEscapeFunctionNodeVisitor.php
@@ -5,16 +5,48 @@ declare(strict_types=1);
 namespace Vural\PHPStanBladeRule\PHPParser\NodeVisitor;
 
 use PhpParser\Node;
+use PhpParser\Node\Stmt\Nop;
 use PhpParser\NodeVisitorAbstract;
+
+use function count;
 
 final class RemoveEscapeFunctionNodeVisitor extends NodeVisitorAbstract
 {
-    public function leaveNode(Node $node): ?Node
+    /** @return null|Node|Node[] */
+    public function leaveNode(Node $node): null|Node|array
     {
-        if (($node instanceof Node\Expr\FuncCall) && $node->name instanceof Node\Name && $node->name->toString() === 'e') {
-            return $node->getArgs()[0]->value;
+        if (! $node instanceof Node\Stmt\Echo_) {
+            return null;
         }
 
-        return null;
+        $funcCallExp = $node->exprs[0];
+
+        if (! $funcCallExp instanceof Node\Expr\FuncCall) {
+            return null;
+        }
+
+        if (! $funcCallExp->name instanceof Node\Name) {
+            return null;
+        }
+
+        if ($funcCallExp->name->toString() !== 'e' && count($funcCallExp->getArgs()) < 1) {
+            return null;
+        }
+
+        if ($funcCallExp->getArgs()[0]->getDocComment() !== null) {
+            $docNop = new Nop();
+            $docNop->setDocComment($funcCallExp->getArgs()[0]->getDocComment());
+
+            return [$docNop, new Node\Stmt\Echo_([$funcCallExp->getArgs()[0]->value])];
+        }
+
+        if ($node->getDocComment() !== null) {
+            $docNop = new Nop();
+            $docNop->setDocComment($node->getDocComment());
+
+            return [$docNop, new Node\Stmt\Echo_([$funcCallExp->getArgs()[0]->value])];
+        }
+
+        return new Node\Stmt\Echo_([$funcCallExp->getArgs()[0]->value]);
     }
 }
