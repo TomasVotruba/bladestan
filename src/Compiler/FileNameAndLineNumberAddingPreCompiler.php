0c3e9ac1036a2918e7d2cdaa0e8a4a2c3f764433
Can Vural <can9119@gmail.com>
2022-04-14 11:08:37 +0200

    Update regex101 link

--- src/Compiler/FileNameAndLineNumberAddingPreCompiler.php
+++ src/Compiler/FileNameAndLineNumberAddingPreCompiler.php
@@ -21,7 +21,7 @@ final class FileNameAndLineNumberAddingPreCompiler
 {
     private const PHP_SINGLE_LINE_COMMENT_REGEX = '#^/\*\*.*?\*/$#';
 
-    /** @see https://regex101.com/r/GnVnZE/1 */
+    /** @see https://regex101.com/r/wB10n8/1 */
     private const PHP_PARTIAL_COMMENT = '#(\* )?@\w+ \b(.*?) \$\w+#';
 
     private string $fileName;
