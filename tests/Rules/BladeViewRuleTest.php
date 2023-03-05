a6869a632af84929d4e4e1242913f7a1b2c41a8b
Tomas Votruba <tomas.vot@gmail.com>
2023-03-05 18:52:32 +0100

    remove unused collecitng visitor

--- tests/Rules/BladeViewRuleTest.php
+++ tests/Rules/BladeViewRuleTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace TomasVotruba\Bladestan\Tests\Rules;
 
+use Iterator;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use TomasVotruba\Bladestan\Rules\BladeRule;
 
 final class BladeViewRuleTest extends RuleTestCase
@@ -15,34 +17,30 @@ final class BladeViewRuleTest extends RuleTestCase
         return self::getContainer()->getByType(BladeRule::class);
     }
 
-    public function testRule(): void
+    /**
+     * @param string[] $analysedFiles
+     * @param mixed[] $expectedErrorsWithLines
+     */
+    #[DataProvider('provideData')]
+    public function testRule(array $analysedFiles, array $expectedErrorsWithLines): void
     {
-        $this->analyse([__DIR__ . '/data/view-factory.php'], [
+        $this->analyse($analysedFiles, $expectedErrorsWithLines);
+    }
+
+    public static function provideData(): Iterator
+    {
+        yield [
+            [__DIR__ . '/data/view-factory.php'],
             [
-                'Binary operation "+" between string and 10 results in an error.',
-                13,
+                ['Binary operation "+" between string and 10 results in an error.', 13],
+                ['Binary operation "+" between string and \'bar\' results in an error.', 13],
+                ['Binary operation "+" between string and 10 results in an error.', 16],
+                ['Binary operation "+" between string and \'bar\' results in an error.', 16],
+                ['Binary operation "+" between string and 10 results in an error.', 19],
+                ['Binary operation "+" between string and \'bar\' results in an error.', 19],
             ],
-            [
-                'Binary operation "+" between string and \'bar\' results in an error.',
-                13,
-            ],
-            [
-                'Binary operation "+" between string and 10 results in an error.',
-                16,
-            ],
-            [
-                'Binary operation "+" between string and \'bar\' results in an error.',
-                16,
-            ],
-            [
-                'Binary operation "+" between string and 10 results in an error.',
-                19,
-            ],
-            [
-                'Binary operation "+" between string and \'bar\' results in an error.',
-                19,
-            ],
-        ]);
+        ];
+
     }
 
     /**
