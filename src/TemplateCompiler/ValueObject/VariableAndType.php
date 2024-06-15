bad0fc00f2fe96da241c915b0e70d532d4b4ddcd
Robert Chettleburgh <rob@evosite.co.uk>
2024-06-15 05:04:08 +0100

    Fix $this type (#90) (#99)

--- src/TemplateCompiler/ValueObject/VariableAndType.php
+++ src/TemplateCompiler/ValueObject/VariableAndType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TomasVotruba\Bladestan\TemplateCompiler\ValueObject;
 
+use PHPStan\Type\ThisType;
 use PHPStan\Type\Type;
 use PHPStan\Type\VerbosityLevel;
 
@@ -26,7 +27,10 @@ final class VariableAndType
     }
 
     public function getTypeAsString(): string
-    {
+    { 
+        if ($this->type instanceof ThisType) {
+            return $this->type->getStaticObjectType()->describe(VerbosityLevel::typeOnly());
+        }
         return $this->type->describe(VerbosityLevel::typeOnly());
     }
 }
