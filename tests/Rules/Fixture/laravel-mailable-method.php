3dd6fd81b88ad91a7c67bf3d56312e15dbfa31c2
William Desportes <williamdes@wdes.fr>
2024-09-02 21:18:08 +0200

    Update tests for Mailable content() function

--- tests/Rules/Fixture/laravel-mailable-method.php
+++ tests/Rules/Fixture/laravel-mailable-method.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace LaravelMailableMethod;
 
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
 
 class MyMailable extends Mailable
 {
@@ -16,5 +17,18 @@ class MyMailable extends Mailable
         return $this->view('foo', [
             'foo' => 'bar',
         ]);
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'foo',
+            with: [
+                'foo' => 'bar',
+            ],
+        );
     }
 }
